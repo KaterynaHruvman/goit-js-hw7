c89ce971f7b274be0b90ea2908c108e59bf7aa96
Ekateryna Gruvman <ekaterynagruvman@Ekaterynas-MacBook-Air.local>
2020-12-10 15:12:41 +0200

    task-1

---
+++ JS/task-1.js
@@ -0,0 +1,10 @@
+"use strict";
+
+
+const numberOfCategories = document.querySelectorAll(".item");
+console.log(`В списке ${numberOfCategories.length} категории`);
+
+const categorieRef = numberOfCategories.forEach((categories) => {
+  console.log(`Категория: ${categories.querySelector("h2").textContent}`);
+  console.log(`Количество элементов: ${categories.querySelectorAll("li").length}`);
+});
